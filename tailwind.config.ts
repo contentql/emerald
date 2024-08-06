f5d086584793a004d182ee5b5063bac7367d7f90
Rahaman <charanm927@gmail.com>
2024-08-06 14:50:48 +0530

    chore: update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,7 @@
 import type { Config } from 'tailwindcss'
 
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-const colors = require('tailwindcss/colors')
-const {
-  default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette')
-
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,98 +10,82 @@ const config: Config = {
     './src/payload/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
+    colors: {
+      primary: 'hsl(var(--primary))',
+      text: 'hsl(var(--text))',
+      secondary: 'hsl(var(--secondary))',
+      background: 'hsl(var(--background))',
+      card: 'hsl(var(--card))',
+      input: 'hsl(var(--input))',
+      transparent: 'transparent',
+      info: {
+        DEFAULT: 'hsl(var(--info))',
+        foreground: 'hsl(var(--info-foreground))',
+      },
+      warning: {
+        DEFAULT: 'hsl(var(--warning))',
+        foreground: 'hsl(var(--warning-foreground))',
+      },
+      danger: {
+        DEFAULT: 'hsl(var(--danger))',
+        foreground: 'hsl(var(--danger-foreground))',
+      },
+      success: {
+        DEFAULT: 'hsl(var(--success))',
+        foreground: 'hsl(var(--success-foreground))',
       },
     },
-    extend: {
-      transformOrigin: {
-        'left-right': '0% 50%',
-      },
-      boxShadow: {
-        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
-      },
-      keyframes: {
-        orbit: {
-          '0%': {
-            transform:
-              'rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)',
-          },
-          '100%': {
-            transform:
-              'rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)',
-          },
-        },
-        progress: {
-          '0%': { transform: ' translateX(0) scaleX(0)' },
-          '40%': { transform: 'translateX(0) scaleX(0.4)' },
-          '100%': { transform: 'translateX(100%) scaleX(0.5)' },
-        },
-        marquee: {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(calc(-100% - var(--gap)))' },
-        },
-        'marquee-vertical': {
-          from: { transform: 'translateY(0)' },
-          to: { transform: 'translateY(calc(-100% - var(--gap)))' },
-        },
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-        scroll: {
-          to: {
-            transform: 'translate(calc(-50% - 0.5rem))',
-          },
-        },
-        'border-beam': {
-          '100%': {
-            'offset-distance': '100%',
-          },
-        },
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      animation: {
-        progress: 'progress 1s infinite linear',
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-        scroll:
-          'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
-        marquee: 'marquee var(--duration) linear infinite',
-        'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
-        orbit: 'orbit calc(var(--duration)*1s) linear infinite',
-      },
+    fontSize: {
+      sm: ['1.4rem', '1.8rem'],
+      base: ['1.6rem', '1.9rem'],
+      lg: ['2.4rem', '2.8rem'],
+      xl: ['3.9rem', '1'],
+    },
+    borderRadius: {
+      none: '0',
+      DEFAULT: '0.4rem',
+      md: '0.8rem',
+      full: '9999px',
+    },
+    spacing: {
+      0: '0',
+      2: '0.2rem',
+      4: '0.4rem',
+      8: '0.8rem',
+      12: '1.2rem',
+      16: '1.6rem',
+      20: '2rem',
+      24: '2.4rem',
+      28: '2.8rem',
+      32: '3.2rem',
+      36: '3.6rem',
+      40: '4rem',
+      44: '4.4rem',
+      48: '4.8rem',
+      52: '5.2rem',
+      56: '5.6rem',
+      60: '6rem',
+      64: '6.4rem',
+      80: '8rem',
+      128: '12.8rem',
+      256: '25.6rem',
+      320: '32rem',
+    },
+    maxWidth: {
+      xs: '32rem', // 320px
+      sm: '38.4rem', // 384px
+      md: '44.8rem', // 448px
+      lg: '51.2rem', // 512px
+      xl: '57.6rem', // 576px
+      '2xl': '67.2rem', // 672px
+      '3xl': '76.8rem', // 768px
+      '4xl': '89.6rem', // 896px
+      '5xl': '102.4rem', // 1024px
+      '6xl': '115.2rem', // 1152px
+      '7xl': '128rem', // 1280px
     },
   },
-  prefix: '',
-  plugins: [
-    require('tailwindcss-animate'),
-    require('@tailwindcss/typography'),
-    addVariablesForColors,
-  ],
-}
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme('colors'))
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
-  )
-
-  addBase({
-    ':root': newVars,
-  })
+  plugins: [require('@tailwindcss/typography')],
 }
 
 export default config
