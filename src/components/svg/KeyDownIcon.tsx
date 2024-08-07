772a12658fcc34aceac419230ae55a30ee9c1b18
charan <charanm927@gmail.com>
2024-08-07 22:10:16 +0530

    chore: style attributes to camelCase

--- src/components/svg/KeyDownIcon.tsx
+++ src/components/svg/KeyDownIcon.tsx
@@ -4,7 +4,7 @@ const KeyDownIcon = () => {
       className='inline-block text-zinc-900'
       width='20px'
       height='20px'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
@@ -12,9 +12,9 @@ const KeyDownIcon = () => {
       <path
         d='M6 9L12 15L18 9'
         stroke='currentColor'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'></path>
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'></path>
     </svg>
   )
 }
