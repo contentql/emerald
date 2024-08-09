7bac5a533b170c19de6f80fcd74517c35e218937
charan <charanm927@gmail.com>
2024-08-09 15:24:46 +0530

    chore: all tags and all authors dynamic

--- src/components/marketing/AllPosts.tsx
+++ src/components/marketing/AllPosts.tsx
@@ -1,85 +1,36 @@
 import Container from '../common/Container'
 import Link from 'next/link'
 
-const AllPosts = () => {
+interface CardDataType {
+  imageUrl: string
+  title: string
+  noOfPosts: number // or specify the exact type if known, e.g., Record<string, number> or something else
+}
+
+const AllPosts = ({ cardData }: { cardData: CardDataType[] }) => {
   return (
     <Container className='py-24'>
       <div className='grid grid-cols-4 gap-8'>
-        <Link
-          href={'/'}
-          className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
-          <div className='flex flex-col items-center justify-center gap-2.5'>
-            <div className='avatar'>
-              <div className='w-20 rounded-full'>
-                <img src='https://elrond.hedwik.io/content/images/size/w600/format/webp/2023/12/thumbnail--1-.svg' />
+        {cardData?.map((card, index) => (
+          <Link
+            key={index}
+            href={'/'}
+            className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
+            <div className='flex flex-col items-center justify-center gap-2.5'>
+              <div className='avatar'>
+                <div className='w-20 rounded-full'>
+                  <img src={card?.imageUrl} />
+                </div>
               </div>
-            </div>
-            <div className='text-base font-semibold sm:text-lg'>
-              Celestia Lilly
-            </div>
-            <p className='font-medium text-neutral-content'>5 posts</p>
-          </div>
-        </Link>
-        <Link
-          href={'/'}
-          className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
-          <div className='flex flex-col items-center justify-center gap-2.5'>
-            <div className='avatar'>
-              <div className='w-20 rounded-full'>
-                <img src='https://elrond.hedwik.io/content/images/size/w600/format/webp/2023/12/thumbnail--1-.svg' />
+              <div className='text-base font-semibold sm:text-lg'>
+                {card?.title}
               </div>
+              <p className='font-medium text-neutral-content'>
+                {card?.noOfPosts} {card?.noOfPosts === 1 ? 'Post' : 'Posts'}
+              </p>
             </div>
-            <div className='text-base font-semibold sm:text-lg'>
-              Celestia Lilly
-            </div>
-            <p className='font-medium text-neutral-content'>5 posts</p>
-          </div>
-        </Link>
-        <Link
-          href={'/'}
-          className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
-          <div className='flex flex-col items-center justify-center gap-2.5'>
-            <div className='avatar'>
-              <div className='w-20 rounded-full'>
-                <img src='https://elrond.hedwik.io/content/images/size/w600/format/webp/2023/12/thumbnail--1-.svg' />
-              </div>
-            </div>
-            <div className='text-base font-semibold sm:text-lg'>
-              Celestia Lilly
-            </div>
-            <p className='font-medium text-neutral-content'>5 posts</p>
-          </div>
-        </Link>
-        <Link
-          href={'/'}
-          className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
-          <div className='flex flex-col items-center justify-center gap-2.5'>
-            <div className='avatar'>
-              <div className='w-20 rounded-full'>
-                <img src='https://elrond.hedwik.io/content/images/size/w600/format/webp/2023/12/thumbnail--1-.svg' />
-              </div>
-            </div>
-            <div className='text-base font-semibold sm:text-lg'>
-              Celestia Lilly
-            </div>
-            <p className='font-medium text-neutral-content'>5 posts</p>
-          </div>
-        </Link>
-        <Link
-          href={'/'}
-          className='rounded-2xl p-4 transition duration-300 ease-in-out hover:bg-secondary'>
-          <div className='flex flex-col items-center justify-center gap-2.5'>
-            <div className='avatar'>
-              <div className='w-20 rounded-full'>
-                <img src='https://elrond.hedwik.io/content/images/size/w600/format/webp/2023/12/thumbnail--1-.svg' />
-              </div>
-            </div>
-            <div className='text-base font-semibold sm:text-lg'>
-              Celestia Lilly
-            </div>
-            <p className='font-medium text-neutral-content'>5 posts</p>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </Container>
   )
