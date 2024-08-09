93c9a3051744ab1077b6b0626d5e40accd5fdc20
charan <charanm927@gmail.com>
2024-08-09 14:49:44 +0530

    chore: create dropDown

---
+++ src/components/common/DropDown.tsx
@@ -0,0 +1,85 @@
+import KeyDownIcon from '../svg/KeyDownIcon'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
+export default function Example() {
+  return (
+    <Menu as='div' className='relative inline-block text-left'>
+      <div>
+        <MenuButton>
+          More
+          <KeyDownIcon />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className='absolute left-1/2 z-10 mt-3 w-56 origin-top-left translate-x-[-50%] rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'>
+        <div className='py-1'>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Subscribe Page
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2  text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Single Author Page
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Single Tag Page
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Left-Aligned Post
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Centered Post
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Recommendations
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Contact
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              404 Page
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href='#'
+              className='block px-4 py-2 text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
+              Documentation
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  )
+}
